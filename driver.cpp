7affe283d0c9cac982a4c5fbec377f85f594a7be
Lauren Parsons <laurenparsons516@gmail.com>
2021-02-16 09:56:38 -0600

    first commit

--- driver.cpp
+++ driver.cpp
@@ -5,4 +5,10 @@ int main(int argc, char** argv)
     Card* KingKrush = new Card("King Krush", 8, 8, 8);
     //(*KingKrush).display();
     KingKrush->display();
+    Card* PortalKeeper = new Card("Portal Keeper", 4, 5, 2);
+    PortalKeeper->display();
+    Card* HeroicInkeeper = new Card("Heroic Inkeeper", 8, 4, 4);
+    HeroicInkeeper->display();
+    Card* ElectricScribe = new Card("Electric Scribe", 6, 6, 4);
+    ElectricScribe->display();
 }
