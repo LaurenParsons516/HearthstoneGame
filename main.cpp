9ded3f24fc6ef67628cc15657b1a9a2c3873b6e7
Lauren Parsons <laurenparsons516@gmail.com>
2021-02-22 23:55:38 -0600

    Allows parsing of json strings

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include "url.cpp"
 
+std::string parse(std::string jsonString) {
+
+    std::string parsedJsonString = "";
+    int counter = 0;
+
+    for (char c : jsonString) {
+        if (c == '[') {
+            counter++;
+        } else if (c == ']') {
+            counter--;
+        }
+
+        if (counter == 0) {
+            break;
+        } else {
+            if (counter == 1 && c == '[') {
+                continue;
+            }
+            parsedJsonString = parsedJsonString + c;
+        }
+    } 
+    return parsedJsonString;
+}
+
 int main() {
-    URL test = URL("http://google.com/");
-    std::cout << test.getContents() << std::endl;
+    //URL cards = URL("https://api.hearthstonejson.com/v1/25770/enUS/cards.json");
+    //std::string jsonString = cards.getContents();
+
+    std::string jsonString = "[{\"fname\":\"val1\", \"lname\": \"val2\", \"sums\":[1,2,3]},{\"fname\":\"val3\", \"lname\":\"val4\"}]";
+
+    std::string jString = parse(jsonString);
+    std::cout << jString << std::endl;
+
+    return 0;
 }
